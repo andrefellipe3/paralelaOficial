6a5dc8a27d35911bc3929d343e3838b392aad68d
Andre Fellipe <andrefellipe2008@hotmail.com>
2024-11-29 00:08:13 -0300

    teste

--- KmeansOMP.c
+++ KmeansOMP.c
@@ -7,8 +7,8 @@
 #define NUM_DIMENSIONS 21   // Número de variáveis ou colunas numéricas
 #define K 5
 #define MAX_ITERATIONS 100
-#define NUM_THREADS 1 //AQUI VOCÊ MUDA O NUMERO DE THREADS
-#define NUM_TEAMS 8
+#define NUM_THREADS 4 //AQUI VOCÊ MUDA O NUMERO DE THREADS
+#define NUM_TEAMS 2
 
 double euclidean_distance(double *a, double *b, int dimensions) {
     double distance = 0.0;
